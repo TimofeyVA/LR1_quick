6fb2ca81685812dba85b4f8275de567db27b0db0
FlaschBang <timofey.vasilev.2001@mail.ru>
2024-04-01 14:17:44 +0500

    init commit

---
+++ build/CMakeFiles/quick.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quick.dir/main.cpp.o"
+  "CMakeFiles/quick.dir/main.cpp.o.d"
+  "quick"
+  "quick.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quick.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
